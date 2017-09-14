9daeb576924931c42d40404cd1fc171491f92094
kev7862 <kevmatite7862@gmail.com>
2017-09-14 12:49:24 +0300

    Resized and renamed my Image, Created a new Activity, and changed my TextView Font style

--- app/src/main/java/kev7862/github/restaurants/MainActivity.java
+++ app/src/main/java/kev7862/github/restaurants/MainActivity.java
@@ -1,26 +1,38 @@
 package kev7862.github.restaurants;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button mfindRestaurantButton;
+    private Button mfindRestaurantsButton;
+    private EditText mLocationEditText;
+    private TextView mtextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mfindRestaurantButton = (Button) findViewById(R.id.findRestaurantButton);
-        mfindRestaurantButton.setOnClickListener(new View.OnClickListener() {
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mfindRestaurantsButton = (Button) findViewById(R.id.findRestaurantButton);
+        mtextView = (TextView) findViewById(R.id.textView);
+        Typeface Pacifico = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
+        mtextView.setTypeface(Pacifico);
+        mfindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
             }
         });
     }
 }
+
