ea41ba5830a8ab4d758b72cfec7457abdc222f3f
kev7862 <kevmatite7862@gmail.com>
2017-09-14 07:15:55 +0300

    Added an EditView to my parent Relativelayout

--- app/src/main/java/kev7862/github/restaurants/MainActivity.java
+++ app/src/main/java/kev7862/github/restaurants/MainActivity.java
@@ -2,12 +2,25 @@ package kev7862.github.restaurants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mfindRestaurantButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mfindRestaurantButton = (Button) findViewById(R.id.findRestaurantButton);
+        mfindRestaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
