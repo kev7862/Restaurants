060e9e66c01e060a8633e00b5cc7a97062f4a95c
kev7862 <kevmatite7862@gmail.com>
2017-09-25 09:13:30 +0300

    Generated a milkshakeActivity then a milkshake list

--- app/src/main/java/kev7862/github/restaurants/RestaurantActivity.java
+++ app/src/main/java/kev7862/github/restaurants/RestaurantActivity.java
@@ -1,38 +1,49 @@
 package kev7862.github.restaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class RestaurantActivity extends AppCompatActivity {
+    private TextView mLocationEditText;
+    private ListView myListView;
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
 
-        //setting-up my listview ArrayList in my restaurants activity
-        ListView myListView = (ListView)findViewById(R.id.myListView);
-        ArrayList<String> myRestaurants = new ArrayList<>();
-        myRestaurants.add("Life of pie");
-        myRestaurants.add("Screen Door");
-        myRestaurants.add("Luc Lac");
-        myRestaurants.add("Sweet Basil");
-        myRestaurants.add("Slappy Cakes");
-        myRestaurants.add("Equinox");
-        myRestaurants.add("Miss Delta's");
-        myRestaurants.add("Andina");
-        myRestaurants.add("Lardo");
-        myRestaurants.add("Portland City Grill");
-        myRestaurants.add("Fat Head's Brewery");
-        myRestaurants.add("Chipotle");
-        myRestaurants.add("Subway");
+        myListView = (ListView) findViewById(R.id.myListView);
+        mLocationEditText = (TextView) findViewById(R.id.locationEditText);
 
-        //Implementing my ArrayAdapter
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter <String>(this, android.R.layout.activity_list_item, myRestaurants);
-        myListView.setAdapter(arrayAdapter);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.select_dialog_singlechoice, restaurants);
+        myListView.setAdapter(adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(RestaurantActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+//        mLocationEditText.setText("Here are all the restaurants near: " + location);
     }
 }
+
