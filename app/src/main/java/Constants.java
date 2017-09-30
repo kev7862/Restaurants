944a035c18ed77fe7f2c514edad15e4fd998a35b
kev7862 <kevmatite7862@gmail.com>
2017-09-30 12:31:37 +0300

    instructing our application to include our credentials in the BuildConfig file. Create a Constant class to reference our credentials in the gradle.properties

---
+++ app/src/main/java/Constants.java
@@ -0,0 +1,13 @@
+import kev7862.github.restaurants.BuildConfig;
+
+/**
+ * Created by matite on 9/30/17.
+ */
+// Creating a Reference to my credentials in the gradle.properties
+public class Constants {
+
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
