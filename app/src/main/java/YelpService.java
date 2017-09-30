93e9d061957db8d8d1404c1d09373e6248dbd394
kev7862 <kevmatite7862@gmail.com>
2017-09-30 18:17:00 +0300

    Using another OkHttp class to construct the URL we'll sent our request to

--- app/src/main/java/YelpService.java
+++ app/src/main/java/YelpService.java
@@ -1,5 +1,6 @@
 import javax.security.auth.callback.Callback;
 
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
@@ -22,6 +23,11 @@ public class YelpService {
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(new SigningInterceptor(consumer))
                 .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+    }
 
     }
 
