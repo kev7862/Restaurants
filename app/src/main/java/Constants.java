fc3b4fff978fac44f2be2754b48bf1a260d52376
kev7862 <kevmatite7862@gmail.com>
2017-09-30 12:43:36 +0300

    Made a few quick changes in my comment section

--- app/src/main/java/Constants.java
+++ app/src/main/java/Constants.java
@@ -3,7 +3,7 @@ import kev7862.github.restaurants.BuildConfig;
 /**
  * Created by matite on 9/30/17.
  */
-// Creating a Reference to my credentials in the gradle.properties
+// Creating a Reference to my credentials in the gradle.properties, instructing our application to include our credentials in the BuildConfig file when it is created
 public class Constants {
 
     public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
