714695b2801c7f7b58c3bf65895a019ec0b4da99
kev7862 <kevmatite7862@gmail.com>
2017-09-30 17:41:38 +0300

    Created an instance of OkHttpOAuthConsumer, and provided it our CONSUMER_KEY and CONSUMER_SECRET

--- app/src/main/java/YelpService.java
+++ app/src/main/java/YelpService.java
@@ -1,5 +1,7 @@
 import javax.security.auth.callback.Callback;
 
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+
 /**
  * Created by matite on 9/30/17.
  */
@@ -11,6 +13,9 @@ import javax.security.auth.callback.Callback;
 public class YelpService {
 
     public static void findRestaurants(String location, Callback callback) {
+
+        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.YELP_CONSUMER_KEY, Constants.YELP_CONSUMER_SECRET);
+        consumer.setTokenWithSecret(Constants.YELP_TOKEN, Constants.YELP_TOKEN_SECRET);
 
     }
 
