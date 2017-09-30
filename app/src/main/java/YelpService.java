64852d42169f54182c392eda0e4cacfcb52bd8e6
kev7862 <kevmatite7862@gmail.com>
2017-09-30 17:34:28 +0300

    define a findRestaurants() method that takes two parameters: The location a user provides, and a callback that will execute when our API request receives a readable response from Yelp.

---
+++ app/src/main/java/YelpService.java
@@ -0,0 +1,17 @@
+import javax.security.auth.callback.Callback;
+
+/**
+ * Created by matite on 9/30/17.
+ */
+
+
+// Contains all  logic necessary for interacting with the Yelp Api.
+//define a findRestaurants() method that takes two parameters: The location a user provides, and a callback that will execute when our API request receives a readable response from Yelp.
+
+public class YelpService {
+
+    public static void findRestaurants(String location, Callback callback) {
+
+    }
+
+}
