32a923cab37c8ae56dc8d25300fccea3c62be638
kev7862 <kevmatite7862@gmail.com>
2017-10-01 17:24:43 +0300

    Creating a new Request with the new url

--- app/src/main/java/YelpService.java
+++ app/src/main/java/YelpService.java
@@ -2,6 +2,7 @@ import javax.security.auth.callback.Callback;
 
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
@@ -27,8 +28,11 @@ public class YelpService {
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
         urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
         String url = urlBuilder.build().toString();
+
+//creating a new Request wih the new URL
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
     }
 
     }
-
-}
